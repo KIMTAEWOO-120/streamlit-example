4f41787ad9dd9c31a1c0ec0cd3147179c4f873cc
KIMTAEWOO-120 <136376152+KIMTAEWOO-120@users.noreply.github.com>
2023-06-13 20:57:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,36 +73,3 @@ plt.tight_layout()
 st.title('Bode Plot')
 st.pyplot(fig)
 
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy import signal
-import streamlit as st
-
-# 개루프 전달함수의 분자와 분모 계수를 설정합니다.
-num = [100]
-den = [1, 5, 6] 
-
-# 개루프 전달함수의 전달함수 객체를 생성합니다.
-sys = signal.TransferFunction(num, den)
-
-# 응답 시간 범위를 설정합니다.
-t = np.linspace(0, 10, 1000)
-
-# unit step 입력에 대한 응답을 계산합니다.
-t, y = signal.step(sys, T=t)
-
-# 그래프를 그리는 함수
-def plot_step_response():
-    fig, ax = plt.subplots()
-    ax.plot(t, y)
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Response')
-    ax.set_title('Step Response of G(s) = 100 / ((s+2)(s+3))')
-    ax.grid(True)
-    return fig
-
-# Streamlit 앱 실행
-st.set_page_config(layout='wide')
-
-fig = plot_step_response()
-st.pyplot(fig)
