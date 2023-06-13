6b7715cb13b7ed8e124501fd6c63d18f86502d96
KIMTAEWOO-120 <136376152+KIMTAEWOO-120@users.noreply.github.com>
2023-06-13 17:51:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,26 @@ import matplotlib.pyplot as plt
 from scipy import signal
 import streamlit as st
 
+# 폐루프 전달함수의 분자와 분모 계수를 설정합니다.
+num = [100]
+den = [1, 5, 106]
+
+# 폐루프 전달함수의 전달함수 객체를 생성합니다.
+sys = signal.TransferFunction(num, den)
+
+# 응답 시간 범위를 설정합니다.
+t = np.linspace(0, 10, 1000)
+
+# unit step 입력에 대한 응답을 계산합니다.
+t, y = signal.step(sys, T=t)
+
+# Streamlit 앱을 생성합니다.
+st.title('Step Response')
+st.line_chart(y, use_container_width=True) import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+import streamlit as st
+
 
 
 # 애플리케이션 제목
