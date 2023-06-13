8616eca8d767ebbb3fab4778a7c6a45a40996914
KIMTAEWOO-120 <136376152+KIMTAEWOO-120@users.noreply.github.com>
2023-06-13 21:11:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,3 +115,25 @@ ax.legend()
 
 # Display the plot using Streamlit
 st.pyplot(fig)
+
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+# 전달함수의 분자와 분모 계수를 설정합니다.
+num = [100]
+den = [1, 5, 6]  # (s+2)(s+3) = s^2 + 5s + 6
+
+# 전달함수 객체를 생성합니다.
+sys = signal.TransferFunction(num, den)
+
+# 시간 범위를 설정합니다.
+t = np.linspace(0, 10, 1000)
+
+# unit step 입력에 대한 응답을 계산합니다.
+t, y = signal.step(sys, T=t)
+
+# Streamlit 애플리케이션을 실행합니다.
+st.title('Step Response')
+st.pyplot(plt.plot(t, y))
