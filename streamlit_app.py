227153b02e9e47f840bef6074c7ab2f5fa24f714
KIMTAEWOO-120 <136376152+KIMTAEWOO-120@users.noreply.github.com>
2023-06-13 17:34:19 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,23 @@ den = [1, 5, 106]
 # 폐루프 전달함수의 전달함수 객체를 생성합니다.
 sys = signal.TransferFunction(num, den)
 
-# 응답 시간 범위를 설정합니다.
-t = np.linspace(0, 10, 1000)
+# 보드선도를 그립니다.
+w, mag, phase = signal.bode(sys)
 
-# unit step 입력에 대한 응답을 계산합니다.
-t, y = signal.step(sys, T=t)
+# 주파수 응답 그래프를 그립니다.
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
+ax1.semilogx(w, mag)
+ax1.set_xlabel('Frequency [rad/s]')
+ax1.set_ylabel('Magnitude [dB]')
+ax1.set_title('Bode Plot - Magnitude')
+
+ax2.semilogx(w, phase)
+ax2.set_xlabel('Frequency [rad/s]')
+ax2.set_ylabel('Phase [degrees]')
+ax2.set_title('Bode Plot - Phase')
+
+plt.tight_layout()
 
 # Streamlit 앱을 생성합니다.
-st.title('Step Response')
-st.pyplot(plt.plot(t, y))
+st.title('Bode Plot')
+st.pyplot(fig)
