2abb7161f0e0a5ed792434aa224b42ec222f27b3
KIMTAEWOO-120 <136376152+KIMTAEWOO-120@users.noreply.github.com>
2023-06-13 17:03:05 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,24 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
 
-"""
-# Welcome to Streamlit!
+# 폐루프 전달함수의 분자와 분모 계수를 설정합니다.
+num = [100]
+den = [1, 5, 106]
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# 폐루프 전달함수의 전달함수 객체를 생성합니다.
+sys = signal.TransferFunction(num, den)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# 응답 시간 범위를 설정합니다.
+t = np.linspace(0, 10, 1000)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# unit step 입력에 대한 응답을 계산합니다.
+t, y = signal.step(sys, T=t)
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# 그래프를 그립니다.
+plt.plot(t, y)
+plt.xlabel('Time')
+plt.ylabel('Response')
+plt.title('Step Response of L(s) = 100 / (s^2 + 5s + 106)')
+plt.grid(True)
+plt.show()
